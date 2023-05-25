6a2f6d729d931f06d42170a3ac4641a6135ba068
RuslanNegoda1989Arjuna <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-05-25 14:23:07 +0000

    Deploying to gh-pages from @ RuslanNegoda1989Arjuna/redux-books@c7cc880ea95bdd7ac07f304766111bd0c8e0c772 🚀

---
+++ static/js/26.9ae24868.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkredux_books=self.webpackChunkredux_books||[]).push([[26],{2026:function(e,s,a){a.r(s);var n=a(184);s.default=function(){return(0,n.jsx)("div",{className:"container",children:(0,n.jsx)("h1",{className:"page-title",children:"Cntacts page"})})}}}]);
+//# sourceMappingURL=26.9ae24868.chunk.js.map
---
+++ static/js/794.4fa605ce.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkredux_books=self.webpackChunkredux_books||[]).push([[794],{1794:function(e,s,n){n.r(s);var a=n(184);s.default=function(){return(0,a.jsx)("div",{className:"container",children:(0,a.jsx)("h1",{className:"page-title",children:"Not found page"})})}}}]);
+//# sourceMappingURL=794.4fa605ce.chunk.js.map
